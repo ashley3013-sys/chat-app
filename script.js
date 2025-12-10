511816f76b1343de8bea37b93bec5964500f0f40
ashley3013-sys <ashley3013@gmail.com>
2025-12-10 19:57:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 // Firebase config (same as before)
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
+  apiKey: "AIzaSyAHLfu2gN-FRYyyXxnVWCwpKNvibC5s7Sg",
+  authDomain: "chat-app-274e3.firebaseapp.com",
+  projectId: "chat-app-274e3",
+  storageBucket: "chat-app-274e3.firebasestorage.app",
+  messagingSenderId: "695289736732",
+  appId: "1:695289736732:web:5f38506a9a5eeef3f839d9",
+  measurementId: "G-SRLH7JPG9V"
 };
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
